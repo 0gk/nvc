627bc403655d87fcd7462b368383fc85ffc64df7
0gk <n@rlab.ru>
2026-03-22 18:39:49 +0300

    Comments and code beautification for treesitter setup

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,25 +1,25 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   branch = 'master', -- older version, nvim 0.10 compartible
-  build = ':TSUpdate',
+  build = ':TSUpdate', -- runs after update the plugin (via :Lazy update), updates installed parsers
   config = function()
     local configs = require('nvim-treesitter.configs')
     configs.setup({
       ensure_installed = {
         'c',
-	'lua',
-	'vim',
-	'vimdoc',
-	'query',
+        'lua',
+        'vim',
+        'vimdoc',
+        'query',
         'python',
-	'javascript',
-	'typescript',
-	'html',
-	'css',
-	'vue',
+        'javascript',
+        'typescript',
+        'html',
+        'css',
+        'vue',
       },
       sync_install = false,
-      auto_install = true,
+      auto_install = true, -- when open a file of a new language, installs a parser don't have yet
       highlight = { enable = true },
     })
   end,
